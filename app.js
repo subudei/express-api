0f823ee8b52969246b58e0fc219e7f6add3fcea7
subudei <milan_cirkovic@yahoo.com>
2023-06-17 14:17:49 +0200

    build(app) : add mongoose & connect mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -10,7 +11,7 @@ const app = express();
 // bodyParser need to be before routes because we want to parse the body before we reach the routes
 app.use(bodyParser.json());
 // use method is a middleware function that will be executed for every incoming request, first argument is the path, second argument is the middleware function that will be executed for incoming requests.
- 
+
 app.use("/api/places", placesRoutes); // => /api/places...
 
 app.use("/api/users", usersRoutes); // => /api/users...
@@ -30,6 +31,46 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(5000);
+//  connect to the database
+const connectUrl = "mongodb+srv://admin-udemy-express:Password123@cluster0.tfzm1c3.mongodb.net/places?retryWrites=true&w=majority"
+const connectConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+ }
+mongoose
+  .connect(
+    connectUrl
+  )
+  .then(() => {
+    app.listen(5000);
+    console.log("MongoDB connected...");
+  })
+  .catch((err) => {
+    console.log(err);
+    // process.exit(1);
+  });
 
-// http://localhost:5000
+
+
+// async mongoose
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(connectUrl);
+//     console.log("MongoDB connected...");
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
+// connectDB();
+// app.listen(5000);
+
+/* 
+
+mongo DB :
+co
+  admin-udemy-express
+  Password123
+*/
